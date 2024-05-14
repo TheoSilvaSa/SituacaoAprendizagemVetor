d37fadc71dd396513930c9d76faed84cee26baba
Sa_Th <Sa_Th@NotebookTheo>
2024-05-14 18:32:47 -0300

    Produto vetorial

--- TrabalhoUjeverson/src/main/java/joaovitor/trabalhoujeverson/Entities/CalculadoraVetorial.java
+++ TrabalhoUjeverson/src/main/java/joaovitor/trabalhoujeverson/Entities/CalculadoraVetorial.java
@@ -22,7 +22,7 @@ public class CalculadoraVetorial {
         
     }
     
-    //THEO
+    //BIER
     public void ProdutoEscalar(){
         
         int a,b;
@@ -90,14 +90,29 @@ public class CalculadoraVetorial {
         }     
     }
     
-    //BIER
+    //THEO
     public void ProdutoVetorial(){
-        /*
-        ESCREVER A SOLUÇÃO NESSA FUNÇÃO, PEDINDO PRO USUÁRIO QUAL O VETOR QUE ELE DESEJA
-        SABER A MAGNITUDE, PODE-SE ALTERAR DA MANEIRA QUE DESEJAR ESSA FUNÇÃO
-        E USANDO OS VETORES DA listaVetor que está aqui nesse objeto ali mais acima
-        ESCREVER NA TELA A PARTIR DESSA FUNÇÃO, POR ISSO VOID
-        */
+        
+        int a,b;
+        
+        System.out.println("Selecione 2 vetores da lista de vetores que será apresentada");
+        for(int i = 0; i < listaVetor.size(); i++){
+            System.out.println("Vetor na Posição " +(i+1)+": "+ listaVetor.get(i).dadosVetor());
+        }
+        
+        System.out.println("Informe o primeiro vetor que deseja utilizar");
+        a = readInt();
+        
+        System.out.println("Informe o segundo vetor que deseja utilizar");
+        b = readInt();
+        
+        Vetor vetor1 = listaVetor.get(a-1);
+        Vetor vetor2 = listaVetor.get(b-1);
+        
+        double x = (vetor1.coordenadaY*vetor2.coordenadaZ - vetor1.coordenadaZ*vetor2.coordenadaY);
+        double y = (vetor1.coordenadaZ*vetor2.coordenadaX - vetor1.coordenadaX*vetor2.coordenadaZ);
+        double z = (vetor1.coordenadaX*vetor2.coordenadaY - vetor1.coordenadaY*vetor2.coordenadaX);
+        System.out.println("O produto vetorial é: ("+x+","+y+","+z+")");
     }
     
     static Scanner scanner = new Scanner(System.in);
